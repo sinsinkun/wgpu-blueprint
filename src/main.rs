666c89e3e1f02165d679ca6985672a084c068c74
Chang Xiao <chang.x1994@gmail.com>
2024-12-31 19:46:11 -0500

    Add time diagnostics to winit

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,73 @@
+use std::time::{Duration, Instant};
+
 use winit::application::ApplicationHandler;
 use winit::dpi::PhysicalSize;
-use winit::event::{KeyEvent, WindowEvent};
+use winit::event::{KeyEvent, WindowEvent, StartCause};
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::keyboard::{PhysicalKey, KeyCode};
 use winit::window::{Window, WindowId};
 
-#[derive(Default)]
+const RENDER_FPS_LOCK: Duration = Duration::from_millis(8);
+
+#[derive(Debug)]
 struct App {
   window: Option<Window>,
+	lifetime: Duration,
+	last_frame: Instant,
+	frame_delta: Duration,
+	last_render_frame: Instant,
+	render_frame_delta: Duration,
+	is_render_frame: bool,
 }
-
+impl Default for App {
+	fn default() -> Self {
+		Self {
+			window: None,
+			lifetime: Duration::from_millis(0),
+			last_frame: Instant::now(),
+			frame_delta: Duration::from_millis(0),
+			last_render_frame: Instant::now(),
+			render_frame_delta: Duration::from_millis(0),
+			is_render_frame: true,
+		}
+	}
+}
 impl ApplicationHandler for App {
+	// initialization
 	fn resumed(&mut self, event_loop: &ActiveEventLoop) {
 		let window_attributes = Window::default_attributes()
 			.with_min_inner_size(PhysicalSize::new(400.0, 300.0))
-			.with_inner_size(PhysicalSize::new(1024.0, 720.0))
-			.with_title("Wgpu App");
+			.with_inner_size(PhysicalSize::new(800.0, 560.0))
+			.with_title("CXGui");
 		match event_loop.create_window(window_attributes) {
 			Ok(win) => {
 				self.window = Some(win);
 			}
 			Err(e) => {
 				println!("Failed to create window: {}", e);
+				event_loop.exit();
 			}
 		};
 		// initialize other things
 	}
+	// system updates
+	fn new_events(&mut self, _evt_loop: &ActiveEventLoop, _cause: StartCause) {
+		// calculate time data
+		let now = Instant::now();
+		self.frame_delta = now - self.last_frame;
+		self.last_frame = now;
+		self.lifetime += self.frame_delta;
+		// restrict rendering pace
+		self.render_frame_delta = now - self.last_render_frame;
+		if self.render_frame_delta > RENDER_FPS_LOCK {
+			self.is_render_frame = true;
+			self.last_render_frame = now;
+			self.window.as_ref().unwrap().request_redraw();
+		} else {
+			self.is_render_frame = false;
+		}
+	}
+	// handle events
 	fn window_event(&mut self, event_loop: &ActiveEventLoop, _win_id: WindowId, event: WindowEvent) {
 		match event {
 			WindowEvent::CloseRequested => {
@@ -49,6 +91,9 @@ impl ApplicationHandler for App {
 			}
 			WindowEvent::RedrawRequested => {
 				// update
+				let fps_1 = 1.0 / self.frame_delta.as_secs_f32();
+				let fps_2 = 1.0 / self.render_frame_delta.as_secs_f32();
+				println!("FPS - Updates: {fps_1}, Renders: {fps_2}");
 				// render
 			}
 			_ => (),
